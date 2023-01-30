179e1d816238558f34c2cfc27e1ace9cdebbe6db
Hongquan Li <hqlisu@gmail.com>
2023-01-30 00:22:22 -0800

    add saving/loading model and plotting result

--- test_gcnmodel.py
+++ test_gcnmodel.py
@@ -13,7 +13,7 @@ data_pd = pd.read_csv('data_full.csv')
 # select the training data
 data_pd_training = data_pd[data_pd['assignment_DNN_taut']=='training']
 # select the first 500 rows
-data_pd_training = data_pd_training[0:500]
+data_pd_training = data_pd_training[0:20000]
 # extract the smiles and labels
 smiles = data_pd_training['SMILES'].to_list()
 labels = data_pd_training['logP'].to_numpy()
@@ -23,18 +23,24 @@ X = featurizer.featurize(smiles)
 # build the dataset
 dataset = dc.data.NumpyDataset(X=X, y=labels)
 # train model
-model = GCNModel(mode='regression',graph_conv_layers=[64,128],dense_layer_size=256,dropout=0.1,
-	n_tasks=1,batch_size=16,learning_rate=0.001)
-loss = model.fit(dataset, nb_epoch=100)
+model = GCNModel(mode='regression',model_dir = 'model',graph_conv_layers=[64,128],dense_layer_size=256,dropout=0.1,
+	n_tasks=1,batch_size=128,learning_rate=0.001)
+loss = model.fit(dataset, nb_epoch=500)
 print(loss)
 
 
 # test the model
 
+# restore the model
+model = GCNModel(mode='regression',graph_conv_layers=[64,128],dense_layer_size=256,dropout=0.1,n_tasks=1,batch_size=128,learning_rate=0.001)
+model.restore(model_dir = 'model')
+
+# load the data
+data_pd = pd.read_csv('data_full.csv')
 # select the validation data
 data_pd_validation = data_pd[data_pd['assignment_DNN_taut']=='validation']
-# select the first 500 rows
-data_pd_validation = data_pd_validation[0:10]
+# # select the first 500 rows
+# data_pd_validation = data_pd_validation[0:500]
 # extract the smiles and labels
 smiles = data_pd_validation['SMILES'].to_list()
 labels = data_pd_validation['logP'].to_numpy()
@@ -48,3 +54,9 @@ prediction = model.predict(dataset)
 ground_truth = data_pd_validation['logP'].to_numpy()
 ground_truth = ground_truth[:,np.newaxis]
 print(np.hstack((ground_truth,prediction)))
+
+import matplotlib.pyplot as plt
+plt.scatter(prediction,data_pd_validation['logP'].to_numpy(),s=0.5)
+plt.xlabel('ground truth')
+plt.ylabel('prediction')
+plt.show()
